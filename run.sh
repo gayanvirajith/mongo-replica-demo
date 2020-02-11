1c4d1fa15f5e2cffd5570dadca6fd184d89f9a55
Gayan <Admin@Gayans-MacBook-Pro.local>
2020-02-11 18:56:09 +0530

    replica shell output

--- run.sh
+++ run.sh
@@ -8,21 +8,15 @@ docker stop mongo-rs0-2 && docker rm mongo-rs0-2
 
 docker stop mongo-rs0-3 && docker rm mongo-rs0-3
 
-docker stop mongo-rs-setup && docker mongo-rs-setup
+docker stop mongo-rs-setup && docker rm mongo-rs-setup
 
-cd mongo-rs0-1 && docker build -t mongo-rs0-1 .
+docker build -t mongo-rs0-1 mongo-rs0-1/
 
-cd ..
+docker build -t mongo-rs0-2 mongo-rs0-2/
 
-cd mongo-rs0-2 && docker build -t mongo-rs0-2 .
+docker build -t mongo-rs0-3 mongo-rs0-3/
 
-cd ..
-
-cd mongo-rs0-3 && docker build -t mongo-rs0-3 .
-
-cd ..
-
-cd setup && docker build -t mongo-rs-setup .
+docker build -t mongo-rs-setup setup/
 
 docker run --net mynet --name mongo-rs0-1 -d -v mongo-rs0-1:/data/db -p 27017:27017 mongo-rs0-1
 
@@ -30,6 +24,6 @@ docker run --net mynet --name mongo-rs0-2 -d -v mongo-rs0-2:/data/db -p 27018:27
 
 docker run --net mynet --name mongo-rs0-3 -d -v mongo-rs0-3:/data/db -p 27019:27017 mongo-rs0-3
 
-docker run --net mynet --name mongo-rs-setup mongo-rs-setup
+docker run --rm --net mynet --name mongo-rs-setup mongo-rs-setup
 
-echo "Done"
+echo "Replica initialization completed!"
