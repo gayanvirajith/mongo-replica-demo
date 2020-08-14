1b2991c56c9ff8ba2a9a40dc0f0b117452d0614b
Gayan Virajith <gayanvirajith@gmail.com>
2020-08-14 19:21:47 +0530

    connections

---
+++ node/test.js
@@ -0,0 +1,43 @@
+const format = require('util').format;
+const MongoClient = require('mongodb').MongoClient;
+
+const mongoUri = format('mongodb://%s,%s,%s/%s?replicaSet=%s',
+  "localhost:27017",
+  "localhost:27018",
+  "localhost:27019",
+  "test",
+  "rs0");
+
+console.log(url);
+
+// const mongoUri = "mongodb://<dbUser>:<dbPassword>@<host1>:<port1>,<host2>:<port2>/<dbName>?replicaSet=<replicaSetName>";
+
+const options = {
+    server: {
+        socketOptions: {
+            autoReconnect: true,
+            keepAlive: 1,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 0
+        }
+    },
+    replSet: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 0
+        }
+    }
+}
+
+MongoClient.connect(mongoUri, options, function(err, db){
+    if(err){
+        console.log(err);
+    } else {
+        db.collections(function(err, collections) {
+            collections.forEach(function(coll) {
+                console.log(coll.s.name);
+            });
+        });
+    }
+});
